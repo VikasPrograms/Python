225cf596413b4dd35c9f03ef971e51971f895b7a
Bade Vikas Vasudeo <119315927+VikasPrograms@users.noreply.github.com>
2022-12-20 02:19:49 +0530

    ML

---
+++ IrisCaseStudy2.py
@@ -0,0 +1,36 @@
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+def MarvellousDecisionTreeClassifier():
+    
+    # 1 : Load the data
+    Dataset = load_iris()
+
+    Data = Dataset.data
+    Target = Dataset.target
+
+    # 2 : Manipulate the Data
+    Data_train, Data_test, Target_train, Target_test = train_test_split(Data, Target, test_size = 0.5)
+
+    Classifier = DecisionTreeClassifier()
+
+    # 3 : Build the model
+    Classifier.fit(Data_train, Target_train)
+
+    # 4 : Test; the model
+    Predictions = Classifier.predict(Data_test)
+
+    Accuracy = accuracy_score(Target_test, Predictions)
+
+    # 5 : Improve -- Missing
+    return Accuracy
+
+def main():
+    Ret = MarvellousDecisionTreeClassifier()
+
+    print("Accuracy of Iris dataset with kNN is : ",Ret * 100)
+
+if __name__ == "__main__":
+    main()
